677ba7d628ce121159231f0e9e30b6804a1cba7a
Kett Lovahr <catusfelony@gmail.com>
2025-07-19 16:15:46 -0300

    Improve Day 01 Part 1

--- day01/day01.fsx
+++ day01/day01.fsx
@@ -1,47 +1,37 @@
-let input = System.IO.File.ReadAllText($"{__SOURCE_DIRECTORY__}/input").Split(", ")
+open System
 
-type Direction =
-| NORTH = 0 | EAST = 1
-| SOUTH = 2 | WEST = 3
-| RIGHT = 1 | LEFT = -1
+type Direction = | North | East | South | West
 
-type Coords =
-    struct
-        val x: int val y: int
-        new(x, y) = {x = x; y = y}
-    end
+let left_of = function
+              | North -> West | West -> South
+              | South -> East | East -> North
 
-type Position =
-    struct 
-        val coords: Coords val dir: Direction
-        new(coords, dir) = {coords = coords; dir = dir}
-    end
+let right_of = function
+               | North -> East | West -> North
+               | South -> West | East -> South
 
-let move (c_pos: Position, inst: string) =
-         let turn = inst[0]
-         let steps = System.Int32.Parse(inst.Substring(1))
-         let dire = match turn with
-                    | 'L' -> if c_pos.dir = Direction.NORTH
-                             then Direction.WEST
-                             else c_pos.dir + Direction.LEFT
-                    | 'R' -> if c_pos.dir = Direction.WEST
-                             then Direction.NORTH
-                             else c_pos.dir + Direction.RIGHT
-                    | _ -> Direction.NORTH //Ideally unreachable
-         let offset = match dire with
-                      | Direction.NORTH -> new Coords(0, -steps)
-                      | Direction.SOUTH -> new Coords(0, steps)
-                      | Direction.WEST -> new Coords(-steps, 0)
-                      | Direction.EAST -> new Coords(steps, 0)
-                      | _ -> new Coords(0, 0) //Ideally unreachable
-         let final_x = c_pos.coords.x + offset.x
-         let final_y = c_pos.coords.y + offset.y
-         new Position (new Coords(final_x, final_y), dire)
+let move_by dir dist = match dir with
+                       | Direction.North -> 0, -dist
+                       | Direction.South -> 0, dist
+                       | Direction.West  -> -dist, 0
+                       | Direction.East  -> dist, 0
 
-let res = input
-          |> Array.fold
-             (fun a e -> move (a, e))
-             (new Position(new Coords(0, 0), Direction.NORTH))
+let addv2 (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
-let final_distance = System.Math.Abs(res.coords.x) + System.Math.Abs(res.coords.y)
-printfn $"{final_distance}"
+let move pos dir (inst:string) =
+     let (turn, steps) = inst.[0], (inst.[1..] |> Int32.Parse)
+     let new_dir = match turn with
+                   | 'L' -> left_of dir
+                   | 'R' -> right_of dir
+                   | _ -> failwith "unexpected direction"
+     let offset = move_by new_dir steps
+     let final = addv2 pos offset
+     final, new_dir
+
+let ((x, y), _) = IO.File.ReadAllText("./day01.txt").Split(", ")
+                  |> Seq.fold
+                     (fun ((x, y), dir) inst -> move (x, y) dir inst)
+                     ((0, 0), Direction.North)
+
+let final_distance = Math.Abs(x) + Math.Abs(y)
+printfn "%i" final_distance
