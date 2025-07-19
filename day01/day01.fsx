1bf7d43dcbca127a6a1a4e739d2bbd0469e271f4
Kett Lovahr <catusfelony@gmail.com>
2025-07-19 18:12:29 -0300

    Day 01 Part 2

--- day01/day01.fsx
+++ day01/day01.fsx
@@ -18,7 +18,7 @@ let move_by dir dist = match dir with
 
 let addv2 (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
-let move pos dir (inst:string) =
+let move pos dir (inst:string) (locations:(int*int) list) =
      let (turn, steps) = inst.[0], (inst.[1..] |> Int32.Parse)
      let new_dir = match turn with
                    | 'L' -> left_of dir
@@ -26,12 +26,30 @@ let move pos dir (inst:string) =
                    | _ -> failwith "unexpected direction"
      let offset = move_by new_dir steps
      let final = addv2 pos offset
-     final, new_dir
+     let new_locations = [1..steps]
+                         |> List.map (fun x -> addv2 pos (move_by new_dir x))
 
-let ((x, y), _) = IO.File.ReadAllText("./day01.txt").Split(", ")
-                  |> Seq.fold
-                     (fun ((x, y), dir) inst -> move (x, y) dir inst)
-                     ((0, 0), Direction.North)
+     final, new_dir, (List.append locations new_locations)
 
-let final_distance = Math.Abs(x) + Math.Abs(y)
-printfn "%i" final_distance
+let input = IO.File.ReadAllText("./day01.txt").Split(", ")
+
+let ((x, y), _, locations) = input
+                             |> Seq.fold
+                                (fun ((x, y), dir, locations) inst -> move (x, y) dir inst locations)
+                                ((0, 0), Direction.North, [])
+
+let rec find_first_repeat list history =
+    match list with
+    | [] -> failwith "didn't find a repeat"
+    | first::rest -> if List.contains first history then
+                         first
+                     else
+                         find_first_repeat rest (List.append history [first])
+
+
+Math.Abs(x) + Math.Abs(y) |> printfn "%i"
+
+let print_position pos = let (x, y) = pos
+                         printfn "(%i, %i)" x y
+
+find_first_repeat locations [] |> (fun (x, y) -> printfn "%i" (Math.Abs(x) + Math.Abs(y)))
